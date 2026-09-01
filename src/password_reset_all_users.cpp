2beebdee474f497d1494b53c49233d5af6942f41
Nathan Baines <nathan.baines@nathcat.net>
2026-09-01 17:45:12 +0100

    Program to request password resets for all users

---
+++ src/password_reset_all_users.cpp
@@ -0,0 +1,54 @@
+
+#include "jdbc/cppconn/connection.h"
+#include "jdbc/cppconn/exception.h"
+#include "jdbc/cppconn/prepared_statement.h"
+#include "jdbc/cppconn/resultset.h"
+#include "jdbc/cppconn/statement.h"
+#include "jdbc/mysql_driver.h"
+#include <memory>
+int main() {
+  sql::Driver *driver = sql::mysql::get_driver_instance();
+  // Attempt to open a connection to the database
+  std::unique_ptr<sql::Connection> db;
+  try {
+    db = std::unique_ptr<sql::Connection>{
+        driver->connect("localhost:3306", "SSO", "")};
+    db->setSchema("SSO");
+  } catch (sql::SQLException &e) {
+    std::cerr << "Couldn't connect to MySQL DB. " << e.what() << std::endl;
+    return 0;
+  }
+
+  std::unique_ptr<sql::ResultSet> users;
+  std::unique_ptr<sql::Statement> stmt;
+  try {
+    stmt = std::unique_ptr<sql::Statement>{db->createStatement()};
+    users = std::unique_ptr<sql::ResultSet>{
+        stmt->executeQuery("SELECT id FROM Users")};
+    stmt->close();
+  } catch (sql::SQLException &e) {
+    std::cerr << "Failed to get users. " << e.what() << std::endl;
+    return 0;
+  }
+
+  try {
+    stmt = std::unique_ptr<sql::Statement>{db->createStatement()};
+    stmt->executeUpdate("START TRANSACTION");
+
+    while (users->next()) {
+      stmt->executeUpdate(std::string("CALL request_password_reset(")
+                              .append(std::to_string(users->getInt("id")))
+                              .append(")"));
+    }
+
+    stmt->executeUpdate("COMMIT");
+    stmt->close();
+  } catch (sql::SQLException &e) {
+    std::cerr << "Failed to execute all reset calls. " << e.what() << std::endl;
+    stmt->executeUpdate("ROLLBACK");
+    stmt->close();
+  }
+
+  std::cout << "Done" << std::endl;
+  return 0;
+}
