cc12449652c25a552af119af85a2846c1259dac3
Nathan Baines <nathan.baines@nathcat.net>
2026-09-01 17:46:46 +0100

    Fixed username in password reset program

--- src/password_reset_all_users.cpp
+++ src/password_reset_all_users.cpp
@@ -13,7 +13,7 @@ int main() {
   try {
     db = std::unique_ptr<sql::Connection>{
         driver->connect("localhost:3306", "SSO", "")};
-    db->setSchema("SSO");
+    db->setSchema("sso");
   } catch (sql::SQLException &e) {
     std::cerr << "Couldn't connect to MySQL DB. " << e.what() << std::endl;
     return 0;
